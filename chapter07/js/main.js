5c24032bd2e800268ed96a1bf27e8684919396c1
mz-hanb <hanb@mz.co.kr>
2018-04-26 13:03:11 +0900

    add chapter07

---
+++ chapter07/js/main.js
@@ -0,0 +1,46 @@
+// console.log( 'main');
+//--- gallery
+var uiGallery = document.getElementById('gallery');
+
+//--- modal
+var modal = document.getElementById('modal-gallery');
+var btnCloseModal = modal.getElementsByClassName('btn-close-modal')[0];
+var _btnCloseModal = modal.getElementsByClassName('btn-close-modal');
+var modalImg = document.querySelectorAll('#modal-gallery .img img')[0];
+
+var imgData = [
+  {title: 'title1', desc: 'desc1'},
+  {title: 'title2', desc: 'desc2'},
+  {title: 'title3', desc: 'desc3'}
+]
+
+// console.log(btnCloseModal);
+// console.log(_btnCloseModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+// ui gallery
+uiGallery.addEventListener('click', function(e){  
+  showImg( ( e.target).getAttribute('src') );
+})
+
+// indigator
+document.getElementById('indigator').addEventListener('click', function(e){  
+  // console.log( e.target.textContent );  
+  changeImgUrl( 'images/img0' + (e.target.textContent) + '.jpg')
+})
+
+function showImg(tgUrl){  
+  showModal(tgUrl);      
+}
+function showModal(url){
+  changeImgUrl(url);
+  modal.classList.remove('hide');
+}
+function closeModal(){
+  modal.classList.add('hide');
+}
+function changeImgUrl(url){
+
+  modalImg.setAttribute('src', url);
+}
