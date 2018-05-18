e9bbf4ce32d0eaf6e06b0f7ceab32b7ecf1d8390
mz-hanb <hanb@mz.co.kr>
2018-05-18 18:24:35 +0900

    modify chapter12( ui, style, js )

--- chapter12-weather/js/weather.js
+++ chapter12-weather/js/weather.js
@@ -1,25 +1,51 @@
 // elements
-var $icon = $('.icon img');
-var $weathers = $('.weathers');
+var $area = $('.area');
+// time
+var $day = $('.time .day');
+var $date = $('.time .date');
+var $currentT = $('.time .current-time');
+
+var $weatherImg = $('.weather img');
+var $weatherDesc = $('.weather .desc');
+
+var $temp = $('.temp .main');
+var $tempMinMax = $('.temp .min-max');
+
+var $preloader = $('.loading');
 
 // values
 var tgUrl = 'http://api.openweathermap.org/data/2.5/weather?';
-
-// var query = 'lat=37.523290&lon=127.036219';
 var query = getWeatherQuery('id', '1835848'); // seoul
-// var query = getWeatherQuery('coord', '37.523290,127.036219');
 var key = '&APPID=8d8ab1717d1d334ec2115059ce15fefd';
 
-// init
+// inits
+getLocation();
 
-getWeather();
+function getLocation() {
+  // console.log( navigator.geolocation ); 현재 지역정보
+  if (navigator.geolocation) {
+    navigator
+      .geolocation
+      .getCurrentPosition(function (pos) {
+        query = getWeatherQuery('coord', pos.coords.latitude + ',' + pos.coords.longitude);
+        getWeather();
+      });
+
+    // default
+  } else {
+    query = getWeatherQuery('id', '1835848'); // seoul
+    getWeather();
+  }
+}
 
 function getWeather() {
+  $preloader.fadeIn();
+  // console.log( query );
   $.ajax({
     url: tgUrl + query + key + '&units=metric',
-    // dataType: 'json',
     success: function (data) {
-      console.dir(data);
+      // console.log( new Date( data.sys.sunrise*1000 ).toString() );
+      $preloader.fadeOut();
       showWeather(data);
     },
     error: function () {
@@ -47,34 +73,53 @@ function getWeatherQuery(type, stValue) {
 }
 
 function showWeather(data) {
-  // name, 
-  var contry = data.sys.country;
-  var cityId = data.id;
-  var cityName = data.name;
-  var temp = {
-    current: data.main.teamp,
-    min: data.main.temp_min,
-    max: data.main.temp_max
-  };
-  var weatherList = data.weather;
-  console.log( weatherList.length );
-  
-  for (var i = 0; i < weatherList.length; i++) {    
-    var weatherData = weatherList[i];    
-    var iconUrl = 'http://openweathermap.org/img/w/' + weatherList[i].icon + '.png';    
-    var str = `
-    <li>
-        <span class="desc">${weatherList[i].description}</span>
-        <span class="icon"><img src=${iconUrl} alt=""></span>          
-      </li>    
-    `;      
-    $weathers.append(str);
-  }
-
-
-
-  var iconUrl = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
-  $icon.attr('src', iconUrl); // weather img       
   console.dir(data);
-  console.log(iconUrl);
+  //--- area
+  $area.text(data.name + ', ' + data.sys.country);
+
+  //--- date
+  var times = new Date()
+    .toString()
+    .split(' ');
+  $day.text(times[0] + 'Day');
+  $date.text(', ' + times[1] + ' ' + times[2]);
+  $currentT.text(', ' + times[4]);
+
+  //--- weather description, image icon
+  var weather = data.weather[0];
+  $weatherImg.attr('src', './imgs/' + getIconUrl(weather.id) + '.svg');
+  $weatherDesc.text(weather.description);
+  // console.log( weather.id ); temperature
+  var temps = data.main;
+  $temp.text(temps.temp + ' ℃');
+  $tempMinMax.text( '( ' + temps.temp_min + ' ℃ ~ ' + temps.temp_max + ' ℃ )');
+
+}
+
+function getIconUrl(weatherId) {
+  var fileName = '';
+  if (weatherId === 800) {
+    return 'day';    
+  }else if(weatherId === 701 ){
+    return 'cloudy';
+  }
+  var tgId = (weatherId + '').substr(0, 1);
+  switch (tgId) {
+    case '2':
+      fileName = 'thunder';
+      break;
+    case '3':
+      fileName = 'rainy-4';
+      break;
+    case '5':
+      fileName = 'rainy-2';
+      break;
+    case '6':
+      fileName = 'snowy-6';
+      break;
+    case '8':
+      fileName = 'cloudy';
+      break;
+  }
+  return fileName;
 }
