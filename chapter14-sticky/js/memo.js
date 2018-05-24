c4324a484118a3d68c0211bb1889997f9df96e8b
mz-hanb <hanb@mz.co.kr>
2018-05-24 18:27:53 +0900

    add chapter14

--- chapter14-sticky/js/memo.js
+++ chapter14-sticky/js/memo.js
@@ -1,66 +1,135 @@
-function Memo($containerEle, pos) {
-  console.log(pos.x, pos.y);
+function Memo($containerEle, pos, thisId) {
+  // console.log('memo> ', pos.x, pos.y);
   this.$memo = $(`
-  <div class="memo">
+  <div class="memo" id=${thisId} data-id=${thisId}>
       <nav>
-        <span class="ui-left">                 
-          <i class="btn-add fas fa-plus"></i>
-          <i class="btn-save far fa-save"></i>
-        </span>
-        <span class="ui-right">
-            <i class="btn-show-list fas fa-list"></i>
-            <i class="btn-remove fas fa-times"></i>        
-        </span>          
+        <p class="btn-wrap-left">          
+          <button class="btn btn-add"><i class="fas fa-plus"></i></button><button class="btn btn-save"><i class="far fa-save"></i></button>
+        </p>
+        <p class="btn-wrap-right">
+        <button class="btn btn-show-list"><i class="fas fa-list"></i></button><button class="btn btn-remove"><i class="fas fa-times"></i></button>        
+        </p>          
       </nav>
       <form action="">
           <textarea name="" id="txt" class="txt"  cols="30" rows="10"></textarea>
       </form>      
-      <div class="sidebar">
+      <ul class="sidebar hide">
+      </ul>    
+    </div>`); 
 
-      </div>    
-    </div>`);
+  this.id = thisId;  
+  this.$container = $containerEle;  
+  this.$nav = this.$memo.find('nav');
+  this.$txt = this.$memo.find('.txt');
+  this.$sidebar = this.$memo.find('.sidebar');
+  this.openSidebar = false;  
 
-  var $thisMemo = this.$memo;
-
-  $containerEle.append(this.$memo);
-  this.$memo.css({
-    top: pos.y,
-    left: pos.x
-  });  
-
-  // console.log(this.$memo);
-
-
-  this.$memo.on('click', 'i', function (e) {
-    var btnName = e.currentTarget.className.split(' ')[0];
-    // $.trigger( btnName );
-    switch (btnName) {
-      case 'btn-add':
-        trigger( 'add' );
-        // $thisMemo.trigger('add');
-        break;
-      case 'btn-save':
-        // $thisMemo.trigger('save');
-        trigger( 'save' );
-        break;
-      case 'btn-show-list':
-        // $thisMemo.trigger('showList');
-        trigger( 'showList' );
-        break;
-      case 'btn-remove':
-        // $thisMemo.trigger('remove');
-        trigger( 'remove' );
-        break;
-    }
-  });
-  // this.$memo.
-  // this.$memo = $containerEle.find('.memo');
-
+  this.addMemo(pos);
+  this.setEvent();
 }
 
 Memo.prototype = {
-  trigger: function( eType ){
-    console.log( eType );
-    // thisMemo.trigger( eType );
+  setEvent: function () {
+    var self = this;   
+    
+    this.$memo.on('mouseover', 'nav', function(e){
+      $(this).parent().draggable();
+    });
+
+    this.$nav.on('click', '.btn', function (e) {
+      var tgBtnName = e.currentTarget.className.split(' ')[1];                      
+      self.applyBtnEvent( tgBtnName );  
+    });
+
+  },
+  applyBtnEvent: function(btnName){
+    // console.log( btnName );
+    switch (btnName) {
+      case 'btn-add':
+        this.trigger('add');
+        break;
+      case 'btn-save':            
+      this.saveContents();
+        break;
+      case 'btn-show-list':            
+        this.toggleSidebar();
+        this.loadContentsList();
+        break;
+      case 'btn-remove':
+        // this.$memo.remove();      
+        this.trigger('remove-memo');
+        break;
+    }
+  },
+  startDrag: function(){
+    var self = this;
+    this.$nav.on('mousemove', function(e){
+      // console.log( e );
+      self.$memo.css({
+        left: e.clientX-125,
+        top: e.clientY-20
+      });
+    });
+  },
+  stopDrag: function(){
+    this.$nav.off('mousemove');    
+  },
+  addMemo: function (pos) {        
+    // console.log( 'memoId> ' + this.id);
+    this.$container.append(this.$memo);
+    this.$memo.css({left: pos.x, top: pos.y});
+  },
+  trigger: function (eType) {
+    // this.$memo.trigger(eType, {id: this.id});
+    this.$memo.trigger({
+      type: eType, 
+      id: this.id
+    });
+  },
+  saveContents: function(){
+    if( this.$txt.val().length === 0 ){
+      alert( '내용을 입력해 주세요.');
+    }else{
+      var fileName = prompt('저장할 파일이름을 입력하세요');    
+      if( fileName ){
+        localStorage.setItem( fileName, this.$txt.val() );   
+      } else {
+        alert( '파일이름을 입력해주세요.');
+      }
+    }    
+  }, 
+  loadContentsList: function(){
+    var files = localStorage.length;
+    var self = this;    
+    var stFiles = '';
+    var currentFileName = '';
+    for( var i = 0; i < files; i++ ){
+      currentFileName = localStorage.key(i);      
+      if( currentFileName.substr(0, 1) != '_'){        
+        stFiles += '<li class="file">'+ localStorage.key(i) + '<a class="icon-trash"><i class="fas fa-trash-alt"></i></a></li>';
+      }     
+    }
+    this.$sidebar.html(stFiles); 
+    
+  }, 
+  openSidebar: function(){
+    this.openSidebar = true;
+    this.$sidebar.addClass('show');
+  }, 
+  closeSidebar: function(){
+    this.openSidebar = false;
+    this.$sidebar.removeClass('show');
+  },
+  toggleSidebar: function(){
+    this.openSidebar = !this.openSidebar;
+    if( this.openSidebar ){
+      this.$sidebar.addClass('show');
+    }else{
+      this.$sidebar.removeClass('show');
+    }
+  }, 
+  removeFile: function(st, idx){
+    localStorage.removeItem(st);
+    $('.file').eq(idx).remove();
   }
 }
